502d312a78be2f3b23332a103a5161866f1ba5f8
sergiorbotelho <sergiorbotelho@gmail.com>
2025-01-30 23:55:47 -0300

    fix: add tryCatch authControllers

--- src/controllers/user/authUserControllers.ts
+++ src/controllers/user/authUserControllers.ts
@@ -1,14 +1,21 @@
 import { Request, Response } from "express";
 import { AuthUserService } from "../../services/user/authUserService";
+import AppError from "../../utils/appError";
 
 export class authUserControllers {
   async handle(req: Request, res: Response) {
-    const { email, password } = req.body;
+    try {
+      const { email, password } = req.body;
+      const authUserService = new AuthUserService();
+      const auth = await authUserService.execute({ email, password });
 
-    const authUserService = new AuthUserService();
-
-    const auth = await authUserService.execute({ email, password });
-
-    return res.status(200).json(auth);
+      return res.status(200).json(auth);
+    } catch (error) {
+      if (error instanceof AppError) {
+        return res.status(error.statusCode).json({ message: error.message });
+      }
+      console.error(error); // Log do erro para depuração
+      return res.status(500).json({ message: "Internal server error" });
+    }
   }
 }
