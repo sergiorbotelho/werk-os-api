f40ed2b7da727e39b974b125a450d835d9708c7b
sergiorbotelho <sergiorbotelho@gmail.com>
2025-02-11 23:58:30 -0300

    fix: add tryCatch createClientControllers

--- src/controllers/client/createClientControllers.ts
+++ src/controllers/client/createClientControllers.ts
@@ -1,36 +1,45 @@
 import { Request, Response } from "express";
 import { CreateClientService } from "../../services/client/createClientService";
+import AppError from "../../utils/appError";
 
 export class CreateClientController {
   async handle(req: Request, res: Response) {
-    const {
-      nome,
-      telefone,
-      cpf,
-      cnpj,
-      cep,
-      endereco,
-      numero,
-      bairro,
-      cidade,
-      uf,
-    } = req.body;
+    try {
+      const {
+        nome,
+        telefone,
+        cpf,
+        cnpj,
+        cep,
+        endereco,
+        numero,
+        bairro,
+        cidade,
+        uf,
+      } = req.body;
 
-    const createClientService = new CreateClientService();
+      const createClientService = new CreateClientService();
 
-    const client = await createClientService.execute({
-      nome,
-      telefone,
-      cpf,
-      cnpj,
-      cep,
-      endereco,
-      numero,
-      bairro,
-      cidade,
-      uf,
-    });
+      const client = await createClientService.execute({
+        nome,
+        telefone,
+        cpf,
+        cnpj,
+        cep,
+        endereco,
+        numero,
+        bairro,
+        cidade,
+        uf,
+      });
 
-    return res.status(201).json(client);
+      return res.status(201).json(client);
+    } catch (error) {
+      if (error instanceof AppError) {
+        return res.status(error.statusCode).json({ message: error.message });
+      }
+      console.error(error); // Log do erro para depuração
+      return res.status(500).json({ message: "Internal server error" });
+    }
   }
 }
