34da45af5df3fcc8d2af109d081ce56454c2091e
sergiorbotelho <sergiorbotelho@gmail.com>
2025-02-27 23:11:07 -0300

    fix: deploy vercel

--- dist/src/prisma/index.js
+++ dist/src/prisma/index.js
@@ -1,8 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const client_1 = require("@prisma/client");
-const globalForPrisma = globalThis;
-const prismaClient = globalForPrisma.prisma || new client_1.PrismaClient();
+const prismaClient = new client_1.PrismaClient();
 exports.default = prismaClient;
-if (process.env.NODE_ENV !== "production")
-    globalForPrisma.prisma = prismaClient;
--- src/prisma/index.ts
+++ src/prisma/index.ts
@@ -1,9 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
-
-const prismaClient = globalForPrisma.prisma || new PrismaClient();
+const prismaClient = new PrismaClient();
 
 export default prismaClient;
-if (process.env.NODE_ENV !== "production")
-  globalForPrisma.prisma = prismaClient;
