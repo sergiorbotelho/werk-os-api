5664c791667dd5cc32ebac6dffd70317a4937ede
Sergio Botelho <sergiorbotelho@gmail.com>
2026-05-27 19:14:18 -0300

    feat: implement authentication validation using Zod in authUserControllers

--- src/controllers/user/authUserControllers.ts
+++ src/controllers/user/authUserControllers.ts
@@ -1,21 +1,31 @@
 import { Request, Response } from "express";
+import { ZodError } from "zod";
+import { badRequest, serverError } from "../../helpers/http";
+import { authUserSchema } from "../../schemas/user";
 import { AuthUserService } from "../../services/user/authUserService";
 import AppError from "../../utils/appError";
 
 export class authUserControllers {
   async handle(req: Request, res: Response) {
     try {
-      const { email, password } = req.body;
+      const params = req.body;
+
+      await authUserSchema.parseAsync(params);
+
       const authUserService = new AuthUserService();
-      const auth = await authUserService.execute({ email, password });
+
+      const auth = await authUserService.execute(params);
 
       return res.status(200).json(auth);
     } catch (error) {
+      if (error instanceof ZodError) {
+        return badRequest(res, error.issues[0].message);
+      }
       if (error instanceof AppError) {
         return res.status(error.statusCode).json({ message: error.message });
       }
-      console.error(error); // Log do erro para depuração
-      return res.status(500).json({ message: "Internal server error" });
+      console.error(error);
+      return serverError(res);
     }
   }
 }
--- src/schemas/user.ts
+++ src/schemas/user.ts
@@ -10,3 +10,13 @@ export const createUserSchema = z.object({
     .string({ error: "Password is required" })
     .min(6, { error: "Password must be at least 6 characters long" }),
 });
+
+export const authUserSchema = z.object({
+  email: z
+    .email({ error: "Invalid email address" })
+    .trim()
+    .min(1, { error: "Email is required" }),
+  password: z
+    .string({ error: "Password is required" })
+    .min(6, { error: "Password must be at least 6 characters long" }),
+});
