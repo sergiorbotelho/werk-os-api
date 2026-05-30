d3298872db1effaeeeec5b062d2746d0b05c1b3f
Sergio Botelho <sergiorbotelho@gmail.com>
2026-05-29 21:04:07 -0300

    feat: refactor UpdateOsControllers to streamline parameter handling and improve readability

--- src/controllers/os/updateOsControlles.ts
+++ src/controllers/os/updateOsControlles.ts
@@ -1,44 +1,32 @@
 import { Request, Response } from "express";
+import { ZodError } from "zod";
+import { badRequest, serverError } from "../../helpers/http";
 import { UpdateOsService } from "../../services/os/updateOsService";
+import AppError from "../../utils/appError";
 
 export class UpdateOsControllers {
   async handle(req: Request, res: Response) {
-    const isIdString = req.params.id;
-    const id = Number(isIdString);
-    const {
-      contato,
-      modeloEquipamento,
-      horaChegada,
-      horaSaida,
-      defeito,
-      defeitoConstatado,
-      solucao,
-      valServico,
-      valMaterial,
-      garantiaPeca,
-      garantiaServico,
-      tipoServico,
-      cliente_id,
-    } = req.body;
-    const updateOsService = new UpdateOsService();
+    try {
+      const isIdString = req.params.id;
+      const id = Number(isIdString);
+      const params = req.body;
 
-    const os = await updateOsService.execute({
-      id,
-      contato,
-      horaChegada,
-      horaSaida,
-      modeloEquipamento,
-      defeito,
-      defeitoConstatado,
-      solucao,
-      valServico,
-      valMaterial,
-      garantiaPeca,
-      garantiaServico,
-      tipoServico,
-      cliente_id,
-    });
+      const updateOsService = new UpdateOsService();
 
-    res.status(201).json(os);
+      const os = await updateOsService.execute({
+        id,
+        ...params,
+      });
+
+      res.status(201).json(os);
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return badRequest(res, error.issues[0].message);
+      }
+      if (error instanceof AppError) {
+        return res.status(error.statusCode).json({ message: error.message });
+      }
+      return serverError(res);
+    }
   }
 }
