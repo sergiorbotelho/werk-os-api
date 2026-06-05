af14fd84d2e63527c6cb28ee411699179803ff08
Sergio Botelho <sergiorbotelho@gmail.com>
2026-06-04 22:04:18 -0300

    feat: build for deploy

--- dist/src/services/customer/getCustomersServices.js
+++ dist/src/services/customer/getCustomersServices.js
@@ -8,6 +8,17 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -38,9 +49,21 @@ class GetCustomersServices {
                     cidade: true,
                     uf: true,
                     cep: true,
+                    _count: {
+                        select: {
+                            os: true,
+                        },
+                    },
                 },
             });
-            return { customers, count };
+            const formattedCustomers = customers.map((_a) => {
+                var { _count } = _a, customer = __rest(_a, ["_count"]);
+                return (Object.assign(Object.assign({}, customer), { totalOs: _count.os }));
+            });
+            return {
+                customers: formattedCustomers,
+                count,
+            };
         });
     }
 }
